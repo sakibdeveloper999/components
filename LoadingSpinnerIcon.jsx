f83ec0164eb5971bfa82f8e9c2c9fd26b5e515e3
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-09 22:51:59 +0600

    add LoadingSpinnerIcon components

---
+++ LoadingSpinnerIcon.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const LoadingSpinnerIcon = () => {
+  return (
+    <div className="flex gap-4 flex-wrap h-screen justify-center items-center">
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/173880/loading-arrows.svg"
+        alt="Loading icon"
+      />
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/491270/loading-spinner.svg"
+        alt="Loading icon"
+      />
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/474682/loading.svg"
+        alt="Loading icon"
+      />
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/199956/loading-loader.svg"
+        alt="Loading icon"
+      />
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/169757/loading-process.svg"
+        alt="Loading icon"
+      />
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/70469/loading.svg"
+        alt="Loading icon"
+      />
+      <img
+        className="w-20 h-20 animate-spin"
+        src="https://www.svgrepo.com/show/448500/loading.svg"
+        alt="Loading icon"
+      />
+    </div>
+  );
+};
+
+export default LoadingSpinnerIcon;
